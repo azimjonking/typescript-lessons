f25a97c4b29aeb9c7d065b6c62451f1e0f2e8448
azimjonking <azimjonjalilov1301@mail.ru>
2025-03-31 20:29:03 +0300

    lesson 13. generic type. class, interface

---
+++ lesson13/index.js
@@ -0,0 +1,17 @@
+"use strict";
+// Typescript generic type
+class List {
+    constructor() {
+        this.elements = [];
+    }
+    add(element) {
+        this.elements.push(element);
+    }
+}
+let list = new List();
+list.add("Salom");
+list.add("Dunyo");
+list.add(12);
+list.add(true);
+console.log(list);
+//# sourceMappingURL=index.js.map
---
+++ lesson13/index.ts
@@ -0,0 +1,48 @@
+// Typescript generic type
+
+// class List<T> {
+//   elements: T[] = [];
+
+//   add(element: T): void {
+//     this.elements.push(element);
+//   }
+// }
+
+// let list = new List<number | string>();
+
+// list.add(12);
+// list.add(23);
+// list.add("asd");
+// list.add("salom");
+
+// console.log(list);
+
+// let list2 = new List<boolean | undefined>();
+
+// list2.add(undefined);
+// list2.add(true);
+
+interface IList<T> {
+  elements: T[];
+  add(element: T): void;
+  // new (name: string, age: number): void;
+}
+
+type ListType = string | number | boolean;
+
+class List implements IList<ListType> {
+  elements: ListType[] = [];
+
+  add(element: ListType): void {
+    this.elements.push(element);
+  }
+}
+
+let list = new List();
+
+list.add("Salom");
+list.add("Dunyo");
+list.add(12);
+list.add(true);
+
+console.log(list);
